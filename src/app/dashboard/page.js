140af5ea238dd124c6bc1ce28a13c4fa49240103
cee-diksha <cee.diksha@gmail.com>
2024-02-19 16:52:40 +0530

    first nextjs commit

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div style={{height: "90vh"}}>
+        dashboard
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/dashboard/settings/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div style={{height: "90vh"}}>
+      settings inside dashboard
+    </div>
+  )
+}
+
+export default page
